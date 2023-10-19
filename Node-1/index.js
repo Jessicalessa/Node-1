83188510be88528b0a2b3aaaf0adeeac968a84fc
Jéssica Estevam Lessa <93428392+Jessicalessa@users.noreply.github.com>
2023-10-19 12:58:53 -0300

    Add files via upload

---
+++ Node-1/index.js
@@ -0,0 +1,16 @@
+//requisições
+const http = require('http');
+
+const hostname = "192.168.32.1";
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+
+    res.end("Olá mundo!");
+})
+
+server.listen(port,hostname,()=>{
+    console.log("Servidor está rodando")
+})
